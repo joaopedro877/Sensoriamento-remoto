4875f31c7c53325282e0bca71c6ec99aa4cce07d
joaopedro877 <68405587+joaopedro877@users.noreply.github.com>
2022-06-21 18:48:28 -0300

    Update script_cat.py

--- script_cat.py
+++ script_cat.py
@@ -7,14 +7,12 @@ Created on Thu Nov 11 11:43:27 2021
 #15/11/2021
 '''juntando os dados de sst em um unico arquivo em netcdf, adicionando uma dimensao de tempo'''
 import xarray
-import numpy as np
-import  pandas as pd
-import netCDF4 
+
 
 
 ''' os arquivos a serem concatenados devem estar na mesma pasta. Deve estar no formato abaixo para funcionar'''
 ''' nao pode ter acento nem til no endereco.Se tiver o python nao consegue achar a pasta ou arquivo'''
-ds = xarray.open_mfdataset(C:/Users/jdebr/Documents/Oceanografia/Disciplinas/Semestre_2021.2/Introducao_a_geotecnologias/dados_level3/sea_level_anomaly_chl/*.nc',combine = 'nested', concat_dim="time")
+ds = xarray.open_mfdataset('C:/Users/jdebr/Documents/Oceanografia/Disciplinas/Semestre_2021.2/Introducao_a_geotecnologias/dados_level3/sea_level_anomaly_chl/*.nc',combine = 'nested', concat_dim="time")
 
 '''dando um nome e salvadano'''
 ds.to_netcdf('sstjan.nc')
